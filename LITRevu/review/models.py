9690fb6c3581a771df7510da0409ff7b7522c25c
PVL06 <pverl06@gmail.com>
2024-12-13 19:43:30 +0100

    remove all images in media and add delete method on ticket model

--- LITRevu/review/models.py
+++ LITRevu/review/models.py
@@ -1,6 +1,8 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
+from pathlib import Path
+from django.conf import settings
 
 
 class Ticket(models.Model):
@@ -9,6 +11,11 @@ class Ticket(models.Model):
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
+
+    def delete(self, *args, **kwargs):
+        path = Path(settings.MEDIA_ROOT / str(self.image))
+        path.unlink()
+        super().delete(*args, **kwargs)
 
 
 class Review(models.Model):
